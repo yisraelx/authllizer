cffcacc2514b54a69f7f63bb2a8f3015f193d4a7
yisraelx <yisraelx@gmail.com>
2019-09-09 15:19:57 +0300

    build: remove unnecessary dependencies (cli and unused)

--- examples/client/ngx/src/app/common/password-match.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PasswordMatchDirective } from './password-match.directive';
-
-describe('PasswordMatchDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PasswordMatchDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- examples/client/ngx/src/app/common/password-strength.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PasswordStrengthDirective } from './password-strength.directive';
-
-describe('PasswordStrengthDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PasswordStrengthDirective();
-    expect(directive).toBeTruthy();
-  });
-});
